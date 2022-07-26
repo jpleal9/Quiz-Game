180b42c632dee11b1d4c42ac1d9c8d4655a8c749
João Leal <jpleal.9@gmail.com>
2022-07-26 18:05:17 +0100

    Deleting some files and renaming others  Changes to be committed: 	deleted:    quiz.py 	renamed:    quiz6.py -> quiz1.py 	modified:   quiz2.py 	deleted:    quiz3.py 	deleted:    quiz4.py 	deleted:    quiz5.py 	deleted:    quiz7.py 	renamed:    quiz8.py -> quiz_not_refracted.py

--- quiz2.py
+++ quiz2.py
@@ -1,12 +1,48 @@
-#Lists option
+# Handle user errors
 
-QUESTIONS = [("When was the last time the New England Patriors where Super Bowl champions?","2019"),
-("What is the secret name of Batman?","Bruce Wayne"),
-("Who pays the song Master of Puppets?","Metallica")]
+from string import ascii_lowercase
 
-for question,result in QUESTIONS:
-    answer = input(f"{question}\n")
-    if answer == result:
-        print("Correct! Ten points to Gryffindor!")
+QUESTIONS = {
+    "When was the last time the New England Patriots where Super Bowl champions?":[
+        "2019","2020","2018","2017"],
+
+    "What is the secret name of Batman?":[
+        "Bruce Wayne","Peter Parker","Clark Kent","Tony Stark"],
+    
+    "Who pays the song Master of Puppets?":[
+        "Metallica","Kate Bush","Queen","Pink Floyd"],
+
+    "What's the name of Marshal Ericksen's father from How I Met Your Mother?":[
+        "Marvin","Marshal","Marcus","Marcellus"]
+
+}
+
+num_correct = 0
+for num, (question,alternatives) in enumerate(QUESTIONS.items(), start=1): #The num helps to keep track of the question number
+    print(f"\nQuestion {num}:")
+    print(f"{question}")
+    correct_answer = alternatives[0]
+    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives))) #Creates a dictionary with the sequence of answers by question
+    for label, alternative in labeled_alternatives.items():
+        print(f" {label}) {alternative}")
+    
+    while(answer_label := input("\nYour answer is...")) not in labeled_alternatives:
+        #The while cycle will keep the input correct, between the 4 possible answers
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
+        #The print will re-question for it to be on the valid input
+
+    answer = labeled_alternatives[answer_label] #The input
+    if answer == correct_answer:
+        print("⭐That is correct!!⭐\n------------------------------\n")
+        num_correct +=1
     else:
-        print(f"Oh geez, {answer!r} is not the correct answer, but {result!r} is...")
+        print(f"😭Unfortunately the answer is {correct_answer!r}😭\n------------------------------\n")
+
+print(f"\nYour score is {num_correct} in {num} questions!\n")
+
+if num_correct == 4: #This one is my own, a way to give a final message to the quiz
+    print("Amazing job! You really are a force to be reckoned!\n\n-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
+elif num_correct in range(2,4):
+    print("Good job, but there is room for improvement!\n\n-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
+else:
+    print("Man you suck, try again!\n\n-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
